beda5fea9abdbe0d3b4f6f9047626683005d5d7d
KwonYongGyu <kyg020726@gmail.com>
2026-01-12 17:09:02 +0900

    제미나이 수정본

---
+++ 01121html/0112-1.js
@@ -0,0 +1,258 @@
+class Npc {
+  #npcList = [
+    {
+      id: 1,
+      name: "다리우스",
+      cls: "W",
+      sx: "man",
+      hp: "1200",
+      mp: "150",
+      str: "85",
+      int: "20",
+      dex: "45",
+      lux: "30",
+      birthDate: "2012.05.23",
+    },
+    {
+      id: 2,
+      name: "아리",
+      cls: "M",
+      sx: "woman",
+      hp: "30",
+      mp: "1,100",
+      str: "15",
+      int: "92",
+      dex: "35",
+      lux: "50",
+      birthDate: "2011.12.13",
+    },
+    {
+      id: 3,
+      name: "애쉬",
+      cls: "A",
+      sx: "woman",
+      hp: "30",
+      mp: "300",
+      str: "40",
+      int: "45",
+      dex: "88",
+      lux: "55",
+      birthDate: "2009.02.21",
+    },
+    {
+      id: 4,
+      name: "제드",
+      cls: "T",
+      sx: "man",
+      hp: "30",
+      mp: "250",
+      str: "50",
+      int: "35",
+      dex: "75",
+      lux: "95",
+      birthDate: "2012.11.13",
+    },
+  ];
+
+  printList() {
+    $(".listDataBlock").empty();
+    this.#npcList.forEach((item) => {
+      // 배열을 순환하면서 item 을 class="frame-2" 태그 안의 자식 태그로 추가한다.
+      $(".listDataBlock").append(this.printRow(item));
+    });
+  }
+  // RPG게임 캐릭터 데이터
+  //   이름, 직업(전사"W",마법사"M",궁수"A",도적"T"), 성별(남자"M",여자"F"), HP, MP, STR, INT, DEX, LUX, 생일
+  // name, cls, sx, hp, mp, str, int, dex, lux, birthDate
+
+  // RPG게임 캐릭터 기능
+  // 출력 print~~()하는 기능
+  // 추가 insert~~(), 수정 update~~(), 삭제 delete~~() 기능
+  // 공격기능 A.attackStr(B); A캐릭터 B캐릭터 STR을 이용하여 공격하면
+  // B캐릭터는 HP가 감소한다.
+  // 마법공격 A.attackInt(B); A캐릭터 B캐릭터 INT을 이용하여 공격하면
+  // B캐릭터는 HP가 감소한다. A캐릭터는 MP가 감소한다.
+  printName(cls) {
+    switch (cls) {
+      case "W":
+        return "전사";
+      case "M":
+        return "마법사";
+      case "A":
+        return "궁수";
+      case "T":
+        return "도적";
+    }
+    return "-";
+  }
+
+
+  printRow(item) {
+    let html = `
+<div class="listDataRow">
+  <div class="listItem">
+    <input type="hidden" class="idClass" value="${item.id}"/>
+    <div class="itemData text-wrapper">${this.printName(item.cls)}</div>
+  </div>
+ 
+  <div class="listItem">
+    <div class="itemData text-wrapper">${item.name}</div>
+  </div>
+</div>`;
+    return html;
+  }
+
+  clearInputBox() {
+    $("#id").val(npc.id);
+    $("#name").val(npc.name);
+    $("#genre").val(npc.cls);
+    $("#sex").val(npc.sx ==="남" ? "man" :  "woman");
+    $("#HP").val(npc.hp);
+    $("#MP").val(npc.mp);
+    $("#STR").val(npc.str);
+    $("#INT").val(npc.int);
+    $("#DEX").val(npc.dex);
+    $("#LUX").val(npc.lux);
+    $("#birthDate").val(npc.birthDate);
+  }
+
+  // [기능] 물리 공격 (A가 B를 공격)
+  attackStr(targetId) {
+    const attackerId = $("#id").val() * 1; // 현재 선택된 NPC
+    if (attackerId === targetId)
+      return alert("자기 자신을 공격할 수 없습니다.");
+
+    const attacker = this.#npcList.find((n) => n.id === attackerId);
+    const target = this.#npcList.find((n) => n.id === targetId);
+
+    if (attacker && target) {
+      target.hp = Math.max(0, Number(target.hp) - Number(attacker.str));
+      alert(
+        `${attacker.name}의 물리 공격! ${target.name}의 남은 HP: ${target.hp}`
+      );
+      this.printList(); // 화면 갱신
+    }
+  }
+
+  // [기능] 마법 공격
+  attackInt(targetId) {
+    const attackerId = $("#id").val() * 1;
+    const attacker = this.#npcList.find((n) => n.id === attackerId);
+    const target = this.#npcList.find((n) => n.id === targetId);
+
+    if (attacker && target) {
+      if (attacker.mp < 50) return alert("마나가 부족합니다.");
+
+      target.hp = Math.max(0, Number(target.hp) - Number(attacker.int));
+      attacker.mp = Number(attacker.mp) - 50; // 마법 사용 시 마나 감소
+      alert(
+        `${attacker.name}의 마법 공격! ${target.name}의 남은 HP: ${target.hp} (남은 MP: ${attacker.mp})`
+      );
+      this.printList();
+    }
+  }
+  setData2InputBox(npc) {
+    // $("#id").val(game.id); 이 값은 화면에 안 보이는 <input id="id" type="hidden" hidden 이 필요하다.
+    // $("#name").val(game.name);
+    $("#id").val(npc.id);
+    $("#name").val(npc.name);
+    $("#cls").val(npc.cls);
+    $("#sx").val(npc.sx);
+    $("#hp").val(npc.hp);
+    $("#mp").val(npc.mp);
+    $("#str").val(npc.str);
+    $("#int").val(npc.int);
+    $("#dex").val(npc.dex);
+    $("#lux").val(npc.lux);
+    $("#birthDate").val(npc.birthDate);
+  }
+
+
+  addNpc() {
+    // 사용자 입력 데이터가 유효한지 검증해야 한다. 유효하지 않으면 경고창 띄우고 리턴;
+    if (!this.checkInputData("add")) {
+      return;
+    }
+    let maxId =
+      this.#npcList.reduce((result, item) => {
+        return result < item.id ? item.id : result;
+      }, 0) + 1;
+    // 새로운 데이터는 id:고유번호 가 필요하다. 그러므로 배열 전체 원소의 id의 최대값 에 +1 한 값을 고유한번호로 가져야한다.
+    // 입력데이터는 JS 객체로 만든다. let JS객체 = {id:고유번호, name:$("#name").val(), genre:"S", grade:"ALL", price:금액, imgUrl:"http://..."};
+    let newNpc = {
+      id: maxId,
+      name: $("#name").val(),
+      cls: $("#cls").val(),
+      sx: $("#sx").val(),
+      hp: $("#hp").val(),
+      mp: $("#mp").val(),
+      str: $("#str").val(),
+      int: $("#int").val(),
+      dex: $("#dex").val(),
+      lux: $("#lux").val(),
+      birthDate: $("#birthDate").val(),
+    };
+    // gameList 배열에 JS객체 를 추가한다. this.#gameList.push(JS객체);
+    this.#npcList.push(newNpc);
+    this.clearInputBox();
+    // gameList 배열정보를 게임목록 화면에 출력한다. this.printList();
+    this.printList();
+  }
+
+  updateNpc() {
+    // 사용자 입력 데이터가 유효한지 검증해야 한다. 유효하지 않으면 경고창 띄우고 리턴;
+    if (!this.checkInputData("update")) {
+      return;
+    }
+    // 입력데이터는 JS 객체로 만든다. let JS객체 = {id:고유번호, name:$("#name").val(), genre:"S", grade:"ALL", price:금액, imgUrl:"http://..."};
+    // gameList 배열에서 JS객체.id 번호랑 같은 원소를 찾는다. let 찾는객체 = this.#gameList.find(() => {});
+    // JS객체를 찾는객체로 바꿔치기 한다.
+    // gameList 배열정보를 게임목록 화면에 출력한다. this.printList();
+  }
+
+  deleteNpc() {
+    // 사용자 입력 데이터가 유효한지 검증해야 한다. 유효하지 않으면 경고창 띄우고 리턴;
+    if (!this.checkInputData("delete")) {
+      return;
+    }
+    // gameList 배열에서 기존의 id 번호랑 같은 원소를 찾는다. let 찾는객체 = this.#gameList.find(() => {});
+    // 찾는객체를 gameList 배열에서 삭제한다.
+    // gameList 배열정보를 게임목록 화면에 출력한다. this.printList();
+  }
+
+  printOneNpc(e) {
+    // 화면의 id 값으로 gameList배열에서 찾는다. let id값 = $("#id").val();, let 찾은원소 = this.#gameList.find(() => {});
+    let selectId = $(e.currentTarget).find(".idClass").val() * 1;
+    console.log(`selectId = ${selectId}`);
+    let findNpc = this.#npcList.find((item) => {
+      return item.id === selectId;
+    });
+    if (findNpc == undefined) {
+      return;
+    }
+    // this.setData2InputBox(찾은원소);
+    this.setData2InputBox(findNpc);
+  }
+}
+
+$(() => {
+  // jquery 실행
+  let nint = new Npc();
+  nint.printList();
+
+  $("#btnAdd").click((e) => {
+    nint.addNpc();
+  });
+
+  $(document).on("click", "#btnUpt", (e) => {
+    nint.updateNpc();
+  });
+
+  $(document).on("click", "#btnDel", (e) => {
+    nint.deleteNpc();
+  });
+
+  $(document).on("click", ".listDataRow", (e) => {
+    nint.printOneNpc(e);
+  });
+});
--- html0112.html/js/0112.js
+++ html0112.html/js/0112.js
@@ -5,12 +5,12 @@ class Npc {
       name: "다리우스",
       cls: "W",
       sx: "남",
-      hp: "1200",
-      mp: "150",
-      str: "85",
-      int: "20",
-      dex: "45",
-      lux: "30",
+      hp: 1200,
+      mp: 150,
+      str: 85,
+      int: 20,
+      dex: 45,
+      lux: 30,
       birthDate: "2012.05.23",
     },
     {
@@ -18,12 +18,12 @@ class Npc {
       name: "아리",
       cls: "M",
       sx: "여",
-      hp: "30",
-      mp: "1,100",
-      str: "15",
-      int: "92",
-      dex: "35",
-      lux: "50",
+      hp: 30,
+      mp: 1100,
+      str: 15,
+      int: 92,
+      dex: 35,
+      lux: 50,
       birthDate: "2011.12.13",
     },
     {
@@ -31,12 +31,12 @@ class Npc {
       name: "애쉬",
       cls: "A",
       sx: "여",
-      hp: "30",
-      mp: "300",
-      str: "40",
-      int: "45",
-      dex: "88",
-      lux: "55",
+      hp: 30,
+      mp: 300,
+      str: 40,
+      int: 45,
+      dex: 88,
+      lux: 55,
       birthDate: "2009.02.21",
     },
     {
@@ -44,34 +44,25 @@ class Npc {
       name: "제드",
       cls: "T",
       sx: "남",
-      hp: "30",
-      mp: "250",
-      str: "50",
-      int: "35",
-      dex: "75",
-      lux: "95",
+      hp: 30,
+      mp: 250,
+      str: 50,
+      int: 35,
+      dex: 75,
+      lux: 95,
       birthDate: "2012.11.13",
     },
   ];
 
+  // [출력] 전체 목록 출력 및 공격 대상 갱신
   printList() {
     $(".listDataBlock").empty();
     this.#npcList.forEach((item) => {
-      // 배열을 순환하면서 item 을 class="frame-2" 태그 안의 자식 태그로 추가한다.
       $(".listDataBlock").append(this.printRow(item));
     });
+    this.updateAttackerSelect(); // 공격 대상 셀렉트박스 업데이트
   }
-  // RPG게임 캐릭터 데이터
-//   이름, 직업(전사"W",마법사"M",궁수"A",도적"T"), 성별(남자"M",여자"F"), HP, MP, STR, INT, DEX, LUX, 생일
-  // name, cls, sx, hp, mp, str, int, dex, lux, birthDate
 
-  // RPG게임 캐릭터 기능
-  // 출력 print~~()하는 기능
-  // 추가 insert~~(), 수정 update~~(), 삭제 delete~~() 기능
-  // 공격기능 A.attackStr(B); A캐릭터 B캐릭터 STR을 이용하여 공격하면
-  // B캐릭터는 HP가 감소한다.
-  // 마법공격 A.attackInt(B); A캐릭터 B캐릭터 INT을 이용하여 공격하면
-  // B캐릭터는 HP가 감소한다. A캐릭터는 MP가 감소한다.
   printName(cls) {
     switch (cls) {
       case "W":
@@ -82,62 +73,30 @@ class Npc {
         return "궁수";
       case "T":
         return "도적";
+      default:
+        return "-";
     }
-    return "-";
   }
-
-  //   printGrade(grade) {
-  //     switch(grade) {
-  //       case "ALL":
-  //         return "전체이용";
-  //       case "18":
-  //         return "18세이상";
-  //       case "13":
-  //         return "13세이상";
-  //     }
-  //     return "-";
-  //   }
 
   printRow(item) {
-    let html = `
-<div class="listDataRow">
-  <div class="listItem">
-    <input type="hidden" class="idClass" value="${item.id}"/>
-    <div class="itemData text-wrapper">${this.printName(item.cls)}</div>
-  </div>
- 
-  <div class="listItem">
-    <div class="itemData text-wrapper">${item.name}</div>
-  </div>
-</div>`;
-    return html;
+    return `
+    <div class="listDataRow" style="border:1px solid #ddd; margin:5px; padding:10px; cursor:pointer;">
+      <input type="hidden" class="idClass" value="${item.id}"/>
+      <strong>[${this.printName(item.cls)}] ${item.name}</strong> 
+      <span>(HP: ${item.hp} / STR: ${item.str})</span>
+    </div>`;
   }
 
+  // [입력창 초기화]
   clearInputBox() {
     $("#id").val(0);
-    $("#name").val("");
-    $("#cls").val("");
-    $("#sx").val("");
-    $("#hp").val("");
-    $("#mp").val("");
-    $("#str").val("");
-    $("#int").val("");
-    $("#dex").val("");
-    $("#lux").val("");
-    $("#birthDate").val("");
+    $("#name, #hp, #mp, #str, #int, #dex, #lux, #birthDate").val("");
+    $("#cls").val("W");
+    $("#sx").val("남");
   }
-  //   clearInputBox() {
-  //     $("#id").val(0);
-  //     $("#name").val("");
-  //     $("#genre").val("A");
-  //     $("#grade").val("ALL");
-  //     $("#price").val(0);
-  //     $("#imgUrl").val("");
-  //   }
 
+  // [데이터 세팅] 목록 클릭 시 입력창으로
   setData2InputBox(npc) {
-    // $("#id").val(game.id); 이 값은 화면에 안 보이는 <input id="id" type="hidden" hidden 이 필요하다.
-    // $("#name").val(game.name);
     $("#id").val(npc.id);
     $("#name").val(npc.name);
     $("#cls").val(npc.cls);
@@ -151,128 +110,119 @@ class Npc {
     $("#birthDate").val(npc.birthDate);
   }
 
-  checkInputData(attack_mode) {
-    // 사용자 입력데이터 검증한다.
-    // 입력데이터가 올바르면 true 리턴
-    // 아니면 false 리턴
-    if (attack_mode === "add" && $("#id").val() != 0) {
-      alert("ID 값이 유효하지 않습니다.");
-      return false;
-    } else if (attack_mode === "update" || mode === "delete") {
-      if ($("#id").val() == 0) {
-        alert("ID 값이 유효하지 않습니다.");
-        return false;
-      } else {
-        return true;
-      }
-    }
-    if ($("#name").val().length < 2 || $("#name").val().length > 30) {
-      alert("이름은 2자~30자까지 입력 가능 합니다.");
-      $("#name").focus();
-      return false;
-    }
-    if (
-      $("#price").val().length < 1 ||
-      $("#price").val() * 1 <= 0 ||
-      $("#price").val() * 1 > 999999
-    ) {
-      alert("가격은 1~999999원 까지 입력 가능 합니다.");
-      $("#price").focus();
-      return false;
-    }
-    if ($("#imgUrl").val().indexOf("http") !== 0) {
-      alert("그림이미지는 http 로 시작해야 합니다.");
-      $("#imgUrl").focus();
-      return false;
-    }
-    return true;
+  // [추가] insert
+  addNpc() {
+    if ($("#name").val() === "") return alert("이름을 입력하세요.");
+
+    let maxId =
+      this.#npcList.reduce((max, item) => Math.max(max, item.id), 0) + 1;
+    let newNpc = this.getInputData(maxId);
+
+    this.#npcList.push(newNpc);
+    this.printList();
+    this.clearInputBox();
   }
 
-  addNpc() {
-    // 사용자 입력 데이터가 유효한지 검증해야 한다. 유효하지 않으면 경고창 띄우고 리턴;
-    if (!this.checkInputData("add")) {
-      return;
+  // [수정] update
+  updateNpc() {
+    let id = $("#id").val() * 1;
+    if (id === 0) return alert("수정할 캐릭터를 목록에서 선택하세요.");
+
+    let index = this.#npcList.findIndex((n) => n.id === id);
+    if (index !== -1) {
+      this.#npcList[index] = this.getInputData(id);
+      this.printList();
+      alert("수정되었습니다.");
     }
-    let maxId =
-      this.#npcList.reduce((result, item) => {
-        return result < item.id ? item.id : result;
-      }, 0) + 1;
-    // 새로운 데이터는 id:고유번호 가 필요하다. 그러므로 배열 전체 원소의 id의 최대값 에 +1 한 값을 고유한번호로 가져야한다.
-    // 입력데이터는 JS 객체로 만든다. let JS객체 = {id:고유번호, name:$("#name").val(), genre:"S", grade:"ALL", price:금액, imgUrl:"http://..."};
-    let newNpc = {
-      id: maxId,
+  }
+
+  // [삭제] delete
+  deleteNpc() {
+    let id = $("#id").val() * 1;
+    if (id === 0) return alert("삭제할 캐릭터를 선택하세요.");
+
+    if (confirm("정말 삭제하시겠습니까?")) {
+      this.#npcList = this.#npcList.filter((n) => n.id !== id);
+      this.printList();
+      this.clearInputBox();
+    }
+  }
+
+  // 입력창의 데이터를 객체로 반환하는 공통 함수
+  getInputData(id) {
+    return {
+      id: id,
       name: $("#name").val(),
       cls: $("#cls").val(),
       sx: $("#sx").val(),
-      hp: $("#hp").val(),
-      mp: $("#mp").val(),
-      str: $("#str").val(),
-      int: $("#int").val(),
-      dex: $("#dex").val(),
-      lux: $("#lux").val(),
+      hp: Number($("#hp").val()),
+      mp: Number($("#mp").val()),
+      str: Number($("#str").val()),
+      int: Number($("#int").val()),
+      dex: Number($("#dex").val()),
+      lux: Number($("#lux").val()),
       birthDate: $("#birthDate").val(),
     };
-    // gameList 배열에 JS객체 를 추가한다. this.#gameList.push(JS객체);
-    this.#npcList.push(newNpc);
-    this.clearInputBox();
-    // gameList 배열정보를 게임목록 화면에 출력한다. this.printList();
+  }
+
+  // [공격] 물리
+  attackStr(targetId) {
+    const attackerId = $("#id").val() * 1;  // 현재 선택된 공격자 id 가져오기
+    if (!attackerId || !targetId) return alert("공격자와 대상을 선택하세요.");
+    if (attackerId === targetId) return alert("자신을 공격할 수 없습니다.");
+
+    const attacker = this.#npcList.find((n) => n.id === attackerId);
+    const target = this.#npcList.find((n) => n.id === targetId);
+
+    target.hp = Math.max(0, target.hp - attacker.str);
+    alert(
+      `${attacker.name}의 공격! ${target.name}의 HP가 ${attacker.str} 감소했습니다.`
+    );
     this.printList();
   }
 
-  updateNpc() {
-    // 사용자 입력 데이터가 유효한지 검증해야 한다. 유효하지 않으면 경고창 띄우고 리턴;
-    if (!this.checkInputData("update")) {
-      return;
-    }
-    // 입력데이터는 JS 객체로 만든다. let JS객체 = {id:고유번호, name:$("#name").val(), genre:"S", grade:"ALL", price:금액, imgUrl:"http://..."};
-    // gameList 배열에서 JS객체.id 번호랑 같은 원소를 찾는다. let 찾는객체 = this.#gameList.find(() => {});
-    // JS객체를 찾는객체로 바꿔치기 한다.
-    // gameList 배열정보를 게임목록 화면에 출력한다. this.printList();
+  // [공격] 마법
+  attackInt(targetId) {
+    const attackerId = $("#id").val() * 1;
+    const attacker = this.#npcList.find((n) => n.id === attackerId);
+    const target = this.#npcList.find((n) => n.id === targetId);
+
+    if (attacker.mp < 50) return alert("마나가 부족합니다 (필요 MP: 50)");
+
+    target.hp = Math.max(0, target.hp - attacker.int);
+    attacker.mp -= 50;
+    alert(
+      `${attacker.name}의 마법! ${target.name}의 HP가 ${attacker.int} 감소했습니다.`
+    );
+    this.printList();
   }
 
-  deleteNpc() {
-    // 사용자 입력 데이터가 유효한지 검증해야 한다. 유효하지 않으면 경고창 띄우고 리턴;
-    if (!this.checkInputData("delete")) {
-      return;
-    }
-    // gameList 배열에서 기존의 id 번호랑 같은 원소를 찾는다. let 찾는객체 = this.#gameList.find(() => {});
-    // 찾는객체를 gameList 배열에서 삭제한다.
-    // gameList 배열정보를 게임목록 화면에 출력한다. this.printList();
+  updateAttackerSelect() {
+    const $select = $("#attackerSelect");
+    $select.empty().append('<option value="0">대상 선택</option>');
+    this.#npcList.forEach((n) => {
+      $select.append(`<option value="${n.id}">${n.name}</option>`);
+    });
   }
 
   printOneNpc(e) {
-    // 화면의 id 값으로 gameList배열에서 찾는다. let id값 = $("#id").val();, let 찾은원소 = this.#gameList.find(() => {});
     let selectId = $(e.currentTarget).find(".idClass").val() * 1;
-    console.log(`selectId = ${selectId}`);
-    let findNpc = this.#npcList.find((item) => {
-      return item.id === selectId;
-    });
-    if (findNpc == undefined) {
-      return;
-    }
-    // this.setData2InputBox(찾은원소);
-    this.setData2InputBox(findNpc);
+    let findNpc = this.#npcList.find((item) => item.id === selectId);
+    if (findNpc) this.setData2InputBox(findNpc);
   }
 }
 
+// 이벤트 연결
 $(() => {
-  // jquery 실행
   let nint = new Npc();
   nint.printList();
 
-  $("#btnAdd").click((e) => {
-    nint.addNpc();
-  });
+  $("#btnAdd").click(() => nint.addNpc());
+  $("#btnUpt").click(() => nint.updateNpc());
+  $("#btnDel").click(() => nint.deleteNpc());
 
-  $(document).on("click", "#btnUpt", (e) => {
-    nint.updateNpc();
-  });
+  $("#btnAtkStr").click(() => nint.attackStr($("#attackerSelect").val() * 1));
+  $("#btnAtkInt").click(() => nint.attackInt($("#attackerSelect").val() * 1));
 
-  $(document).on("click", "#btnDel", (e) => {
-    nint.deleteNpc();
-  });
-
-  $(document).on("click", ".listDataRow", (e) => {
-    nint.printOneNpc(e);
-  });
+  $(document).on("click", ".listDataRow", (e) => nint.printOneNpc(e));
 });
